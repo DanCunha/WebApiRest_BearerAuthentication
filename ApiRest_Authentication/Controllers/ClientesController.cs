b1a6a07265270fb88acf99059b76d70e075c6e44
Daniel Cunha <cunhacdaniel@gmail.com>
2018-07-23 22:25:04 -0400

    Commit Inicial

---
+++ ApiRest_Authentication/Controllers/ClientesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiRest_Authentication.Controllers
+{
+    [Authorize]
+    [Route("clientes")]
+    public class ClientesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var dict = new Dictionary<string, string>();
+
+            HttpContext.User.Claims.ToList()
+               .ForEach(item => dict.Add(item.Type, item.Value));
+
+            return Ok(dict);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("getteste")]
+        public IActionResult Teste()
+        {
+            try
+            {
+                return Ok("Sucesso");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message + "|" + e.InnerException);
+            }
+        }
+    }
+}
---
+++ ApiRest_Authentication/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ApiRest_Authentication.Models;
+using ApiRest_Authentication.Helpers;
+
+namespace ApiRest_Authentication.Controllers
+{
+    [Route("token")]
+    [AllowAnonymous]
+    public class TokenController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody]LoginInputModel inputModel)
+        {
+            if (inputModel.Username != "basis" && inputModel.Password != "12345678")
+                return Unauthorized();
+
+            var token = new JwtTokenBuilder()
+                                .AddSecurityKey(JwtSecurityKey.Create("fiver-secret-key"))
+                                .AddSubject("james bond")
+                                .AddIssuer("Fiver.Security.Bearer")
+                                .AddAudience("Fiver.Security.Bearer")
+                                .AddClaim("MembershipId", "111")
+                                .AddExpiry(1)
+                                .Build();
+
+            //return Ok(token);
+            return Ok(token.Value);
+        }
+    }
+}
